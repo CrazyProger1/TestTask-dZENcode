34394469c567b4b71d8640fe60ca0a495b3ae6c3
CrazyProger1 <crazyproger1@gmail.com>
2024-05-10 17:45:20 +0300

    Fix: listing likes

--- server/server/apps/comments/views/likes.py
+++ server/server/apps/comments/views/likes.py
@@ -1,6 +1,6 @@
 from rest_framework import permissions
 
-from server.apps.comments.services import get_all_likes
+from server.apps.comments.services import get_all_likes, get_comment_likes
 from server.apps.comments.serializers import CommentLikeSerializer
 
 from server.apps.comments.views.comments import CommentParentViewSet
@@ -10,6 +10,10 @@ class CommentLikeViewSet(CommentParentViewSet):
     queryset = get_all_likes()
     serializer_class = CommentLikeSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        comment = self.get_parent_object()
+        return get_comment_likes(comment=comment)
 
     def perform_create(self, serializer):
         serializer.save(comment=self.get_parent_object())
